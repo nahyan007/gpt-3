24888c749578ece9e029d7e7606b0a9867039ac6
nahyan007 <shorifrex@gmail.com>
2023-06-20 20:28:04 +0600

    blog container

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -3,8 +3,18 @@ import './CallToAction.css'
 
 const CallToAction = () => {
     return (
-        <div>
-            
+        <div className='gpt3__cta'>
+            <div className="gpt3__cta-content">
+                <p>
+                Request Early Access to Get Started
+                </p>
+                <h3>
+                Request Early Access to Get Started
+                </h3>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button type='button'>Get Started</button>
+            </div>
         </div>
     );
 };
